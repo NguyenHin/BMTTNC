d0eba7b9be9571e063fa4681314baa799241d20d
NguyenHin <nguyenhien8148@gmail.com>
2025-06-06 11:22:57 +0700

    .

--- Buoi1/ex4.py
+++
@@ -1,7 +0,0 @@
-#tạo ds rỗng để lưu KQ
-j=[]
-#Duyệt qua tất cả số trong đoạn từ 2000->3200, xem có % 7 và không phải bội số của 5
-for i in range(2000, 3200):
-    if(i % 7 == 0) and (i % 5 != 0):
-        j.append(str(i))
-print (",".join(j))
